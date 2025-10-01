a2603cf3b6f30a112f051c09d749c707d5b052ab
Tomas Juraska <juraskat@amazon.com>
2025-09-30 17:34:00 -0700

    Updated http pool settings

--- utils.py
+++ utils.py
@@ -22,7 +22,7 @@ loglevels = {
 }
 
 # Configure HTTP requests
-http = urllib3.PoolManager(num_pools=128, maxsize=512, timeout=3, retries=urllib3.Retry(total=0, redirect=True))
+http = urllib3.PoolManager(num_pools=2, maxsize=10, timeout=3, retries=urllib3.Retry(total=0, redirect=True))
 
 # SCTE messages
 segmentationmessagemap = {
